cbae67711a58e3b50407698b59f1394ced732ef9
marstepanyan <102877297+marstepanyan@users.noreply.github.com>
2023-07-30 03:37:24 +0400

    Add files via upload

--- Task5/AlgorithmsAndDataStructures /Sorting/BubbleSort.py
+++ Task5/AlgorithmsAndDataStructures /Sorting/BubbleSort.py
@@ -1,1 +1,19 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        # Set flag to False to check if any swaps occur in this iteration
+        swapped = False
 
+        for j in range(n-1-i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+
+        # If no swaps occurred, the array is already sorted
+        if not swapped:
+            break
+
+
+arr1 = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(arr1)
+print(arr1)
